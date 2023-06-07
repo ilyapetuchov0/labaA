155ced74a1b344b27512806c2f23e89f0242716f
ilya <ilyapetuchov0@gmail.com>
2023-06-08 02:03:43 +0300

    Fix

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main()
     cin >> a >> b;
     cout << "A + B = " << a + b << '\n'
          << "A - B = " << a - b << '\n'
-         << "A * B = " << a * b << '\n';
+         << "A / B = " << a * b << '\n';
 
     return 0;
 }
