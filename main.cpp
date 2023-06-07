18043e0fec5767ec33ac7a237d3dd0d8d42daadc
ilya <ilyapetuchov0@gmail.com>
2023-06-08 02:02:03 +0300

    Fix bags

--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 int main()
 {
-    int a; int b;
-
-    std::cin >> a;
-    std::cin >> b;
-
-    std::cout << a - b;
+    int a, b;
+    cout << "Enter A and B: ";
+    cin >> a >> b;
+    cout << "A + B = " << a + b << '\n'
+         << "A - B = " << a - b << '\n'
+         << "A * B = " << a * b << '\n';
 
     return 0;
 }
